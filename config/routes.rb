065b82ccc096c1366a5c2491ec924fd9f28a1513
jebediahelliott <jebediahelliott@gmail.com>
2019-07-12 10:26:15 -0400

    Adjust fallback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  get '*path', to: "application#fallback_index_html"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
