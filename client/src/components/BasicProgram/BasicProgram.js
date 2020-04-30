f996c4a7b06a28cc12ee07b161e741a2f90cb73c
jebediahelliott <jebediahelliott@gmail.com>
2020-04-30 19:14:49 -0400

    Update dependencies and make content changes

--- client/src/components/BasicProgram/BasicProgram.js
+++ client/src/components/BasicProgram/BasicProgram.js
@@ -21,13 +21,13 @@ class BasicProgram extends Component {
         <div className='staticPage'>
           <h1>Basic Obedience Program</h1>
           <p>
-            This course is designed to make your dog a better family member and to help gain control in real life situations. the Basic Obedience Program consists of four private lessons and three group lessons covering heel, wait, sit, stay, down, and recall. It also includes problem areas such as jumping, chewing, digging, etc.
+            This course is designed to make your dog a better family member and to help gain control in real life situations. the Basic Obedience Program consists of 7 private lessons and 4 free group classes covering heel, wait, sit, stay, down, and recall. It also includes problem areas such as jumping, chewing, digging, etc.
           </p>
           <p>
             The Basic Obedience Program is for dogs four months and older of any breed.
           </p>
           <p>
-            Price: $325
+            Price: $450
           </p>
         </div>
       </div>
--- client/src/components/GraduateProgram/GraduateProgram.js
+++ client/src/components/GraduateProgram/GraduateProgram.js
@@ -19,11 +19,11 @@ class GraduateProgram extends Component {
           menu={<Navigation />}
         />
         <div className='staticPage'>
-        <h1>Graduaate Program</h1>
+        <h1>Graduate Program</h1>
           <p>
-            For those who would like to establish more control with their dogs, the Graduate Program takes the Basic Program a couple of steps further and teaches the finish, stand for exam, and sit, stand, and down in motion. The Graduate Program cositsts of 7 private lessons and 5 group lessons. For dogs 4 months and older of any breed.
+            For those who would like to establish more control with their dogs, the Graduate Program takes the Basic Program a couple of steps further and teaches the finish, stand for exam, and sit, stand, and down in motion. The Graduate Program cositsts of 10 private lessons and 7 group classes. For dogs 4 months and older of any breed.
           </p>
-          <p>Price: $450</p>
+          <p>Price: $650</p>
         </div>
       </div>
     )
